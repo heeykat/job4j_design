da099b96c382351cbb7ab443f62c3d529005007f
ekaterinasavcuk <heeykat@gmail.com>
2024-10-17 20:19:10 +0200

    2. Создать контейнер на базе связанного списка  [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,81 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements SimpleLinked<E> {
+
+    private int size;
+    private int modCount;
+    private Node<E> head;
+
+    @Override
+    public void add(E value) {
+        if (head == null) {
+            head = new Node<>(value, null);
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node<>(value, null);
+        }
+        ++size;
+        ++modCount;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> current = head;
+        int i = 0;
+        while (i != index) {
+            current = current.next;
+            i++;
+        }
+        return current.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private int expectedModCount = modCount;
+            private Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                checkModification();
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                checkModification();
+                Node<E> rst = current;
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                current = rst.next;
+                return rst.item;
+            }
+
+            private void checkModification() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
