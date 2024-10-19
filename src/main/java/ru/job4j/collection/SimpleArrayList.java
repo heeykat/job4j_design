5a806a4ed7f122a085043d635840fbb26d29b36f
ekaterinasavcuk <heeykat@gmail.com>
2024-10-19 14:53:19 +0200

    1. Динамический список на массиве. [#158 #517965]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -76,7 +76,6 @@ public class SimpleArrayList<T> implements SimpleList<T> {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                checkModification();
                 return container[index++];
             }
 
