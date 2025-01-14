9cc7d9480138e90483b033f744dc1e9d1fb78c66
ekaterinasavcuk <heeykat@gmail.com>
2025-01-14 19:54:33 +0100

    9.0. Файлы и директории. Класс File [#252491 #528443]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -11,9 +11,9 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Это не директория: %s", file.getAbsoluteFile()));
         }
-        System.out.printf("Размер директории: %.2f%n", file.getTotalSpace() / 1073741824.0);
+        System.out.printf("Размер директории: %.2f Gb%n", file.getTotalSpace() / 1073741824.0);
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getName() + " " + String.format("%.2f", subfile.length() / 1024.0) + " Mb");
+            System.out.printf("%s: %.2f Kb%n", subfile.getName(), subfile.length() / 1024.0);
         }
     }
 }
