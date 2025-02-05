215ff19c3e6e6ff5b8b5114bf5eb644e6451351c
ekaterinasavcuk <heeykat@gmail.com>
2025-02-05 11:28:21 +0100

    3. Slf4j - вывод переменных. [#268852 #531350]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -10,13 +10,15 @@ public class UsageLog4j {
     public static void main(String[] args) {
         int birthDate = 24012010;
         boolean isWomen = true;
+        long phone = 8903678456L;
         byte classNumber = 9;
         char classLetter = 'b';
         float grade = 4.13F;
+        double fullGrade = 4.1284543509;
         short hours = 564;
 
-        LOG.debug("Student info birthDate : {}, isWomen : {}, classNumber : {},"
-                + "classLetter : {}, grade : {}, hours : {}", birthDate, isWomen, classNumber,
-                classLetter, grade, hours);
+        LOG.debug("Student info birthDate : {}, isWomen : {}, phone : {}, classNumber : {}, "
+                + "classLetter : {}, grade : {}, fullGrade : {}, hours : {}", birthDate, isWomen, phone, classNumber,
+                classLetter, grade, fullGrade, hours);
     }
 }
