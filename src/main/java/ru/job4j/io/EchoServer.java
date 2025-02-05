bc15bbc2119a13ac9fbd831b03aeb3accde7e3c6
ekaterinasavcuk <heeykat@gmail.com>
2025-02-05 20:13:42 +0100

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,6 +1,9 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,14 +16,23 @@ public class EchoServer {
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String firstLine = input.readLine();
-                    if (firstLine != null && firstLine.contains("msg=Bye")) {
-                        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        output.write("The server is shutting down\r\n\r\n".getBytes());
-                        output.flush();
-                        server.close();
-                        break;
-                    }
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    if (firstLine != null && firstLine.contains("msg=")) {
+                        String msg = firstLine.split("=")[1].split(" ")[0];
+                        if ("Exit".equals(msg)) {
+                            output.write("The server is shutting down\r\n\r\n".getBytes());
+                            output.flush();
+                            server.close();
+                            break;
+                        } else if ("Hello".equals(msg)) {
+                            output.write("Hello, dear friend.\r\n\r\n".getBytes());
+                        } else if ("Any".equals(msg)) {
+                            output.write("What\r\n\r\n".getBytes());
+                        } else {
+                            String answer = msg + "\r\n\r\n";
+                            output.write(answer.getBytes());
+                        }
+                    }
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         System.out.println(string);
                     }
